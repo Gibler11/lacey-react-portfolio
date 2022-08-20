0003fcc7a19889d38d8ca70a504107bc00cfd800
lacey gibler <maxsonlacey@gmail.com>
2022-08-20 08:23:43 -0400

    work with tutor

---
+++ photo/src/components/Contact/index.js
@@ -0,0 +1,16 @@
+import React, {useState} from "react"
+
+import {validateEmail} from "../../utils/helpers"
+
+function Contact (){
+    const [formState, setFormState] = useState ({
+        name: "", 
+        email: "",
+        message: "",
+     });
+
+     const [errorMessage, setErrorMessage] = useState ("");
+     const {name, email, message} = formState;
+
+     
+}
