f0b370e15afef005995f6499406b350f60bf8fdb
Lacey Gibler01~ <maxsonlacey@gmail.com>
2022-08-26 22:11:11 -0400

    worked with tutor

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Navigation from "../Navigation";
+import laceyprofile from "../../assets/images/laceyprofile.jpg";
 
 function Header() {
     return (
-        <>
+        <header>
         <Navigation/>
-        </>
+            <h1> Lacey Gibler </h1>
+            <img src= {laceyprofile} alt="Lacey Gibler headshot" ></img>
+        </header>
     )
 }
 
